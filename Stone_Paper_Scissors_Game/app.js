cafefc9c86f731655291ea67b935efd04b92afc9
Swapnil Pote <138121134+poteswapnil555@users.noreply.github.com>
2024-09-27 18:57:27 +0530

    Add files via upload

---
+++ Stone_Paper_Scissors_Game/app.js
@@ -0,0 +1,83 @@
+let userScore = 0;
+let compScore = 0;
+
+// ----------------------------------------------------------------------------------------
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+// ----------------------------------------------------------------------------------------
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
+
+// ----------------------------------------------------------------------------------------
+
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3); //   Math.floor  ----> To Remove Decimal Values
+  return options[randIdx];
+};
+
+// ----------------------------------------------------------------------------------------
+
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+// ----------------------------------------------------------------------------------------
+
+
+const playGame = (userChoice) => {
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+// ----------------------------------------------------------------------------------------
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+
+/* Steps Followed
+
+step 1)  ---->  User Choice Functionality
+step 2)  ---->  Computer Choice Functionality
+step 3)  ---->  Trial Play Game Functionality ( Checking )
+step 4)  ---->  Game Draw Functionality 
+step 5)  ---->  Actual Play Game Functionality
+step 6)  ---->  Show Winner Functionality
+                                                                                           */
